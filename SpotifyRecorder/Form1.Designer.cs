06087443145c8cf394c066a591d797ea4264c4dd
TheBitBrine <blacknomex08@gmail.com>
2017-11-23 16:46:12 +0400

    The Birth v0.1

---
+++ SpotifyRecorder/Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace SpotifyRecorder
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.RecordButton = new System.Windows.Forms.Button();
+            this.FetchTitleTimer = new System.Windows.Forms.Timer(this.components);
+            this.FetchInfoTimer = new System.Windows.Forms.Timer(this.components);
+            this.TrackWatchdog = new System.Windows.Forms.Timer(this.components);
+            this.PlaylistModeCheckBox = new System.Windows.Forms.CheckBox();
+            this.SongNameLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.UI_UpdaterTimer = new System.Windows.Forms.Timer(this.components);
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.ArtworkPB = new System.Windows.Forms.PictureBox();
+            this.GitHubRepLink = new System.Windows.Forms.LinkLabel();
+            ((System.ComponentModel.ISupportInitialize)(this.ArtworkPB)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // RecordButton
+            // 
+            this.RecordButton.Location = new System.Drawing.Point(12, 316);
+            this.RecordButton.Name = "RecordButton";
+            this.RecordButton.Size = new System.Drawing.Size(97, 23);
+            this.RecordButton.TabIndex = 0;
+            this.RecordButton.Text = "Record";
+            this.RecordButton.UseVisualStyleBackColor = true;
+            this.RecordButton.Click += new System.EventHandler(this.Record_Click);
+            // 
+            // FetchTitleTimer
+            // 
+            this.FetchTitleTimer.Enabled = true;
+            this.FetchTitleTimer.Interval = 150;
+            this.FetchTitleTimer.Tick += new System.EventHandler(this.FetchTitleTimer_Tick);
+            // 
+            // FetchInfoTimer
+            // 
+            this.FetchInfoTimer.Enabled = true;
+            this.FetchInfoTimer.Interval = 250;
+            // 
+            // TrackWatchdog
+            // 
+            this.TrackWatchdog.Enabled = true;
+            this.TrackWatchdog.Interval = 250;
+            this.TrackWatchdog.Tick += new System.EventHandler(this.TrackWatchdog_Tick);
+            // 
+            // PlaylistModeCheckBox
+            // 
+            this.PlaylistModeCheckBox.AutoSize = true;
+            this.PlaylistModeCheckBox.Checked = true;
+            this.PlaylistModeCheckBox.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.PlaylistModeCheckBox.Location = new System.Drawing.Point(115, 320);
+            this.PlaylistModeCheckBox.Name = "PlaylistModeCheckBox";
+            this.PlaylistModeCheckBox.Size = new System.Drawing.Size(88, 17);
+            this.PlaylistModeCheckBox.TabIndex = 2;
+            this.PlaylistModeCheckBox.Text = "Playlist Mode";
+            this.PlaylistModeCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // SongNameLabel
+            // 
+            this.SongNameLabel.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SongNameLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.SongNameLabel.Location = new System.Drawing.Point(12, 275);
+            this.SongNameLabel.Name = "SongNameLabel";
+            this.SongNameLabel.Size = new System.Drawing.Size(260, 23);
+            this.SongNameLabel.TabIndex = 5;
+            this.SongNameLabel.Text = "TheBitBrine\'s Spotify Recorder v0.1";
+            this.SongNameLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label1
+            // 
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.Location = new System.Drawing.Point(12, 305);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(260, 1);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "label1";
+            // 
+            // UI_UpdaterTimer
+            // 
+            this.UI_UpdaterTimer.Enabled = true;
+            this.UI_UpdaterTimer.Interval = 350;
+            this.UI_UpdaterTimer.Tick += new System.EventHandler(this.UI_UpdaterTimer_Tick);
+            // 
+            // folderBrowserDialog1
+            // 
+            this.folderBrowserDialog1.RootFolder = System.Environment.SpecialFolder.MyComputer;
+            // 
+            // ArtworkPB
+            // 
+            this.ArtworkPB.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.ArtworkPB.Image = global::SpotifyRecorder.Properties.Resources.vinyl;
+            this.ArtworkPB.InitialImage = global::SpotifyRecorder.Properties.Resources.vinyl;
+            this.ArtworkPB.Location = new System.Drawing.Point(12, 12);
+            this.ArtworkPB.Name = "ArtworkPB";
+            this.ArtworkPB.Size = new System.Drawing.Size(258, 258);
+            this.ArtworkPB.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.ArtworkPB.TabIndex = 4;
+            this.ArtworkPB.TabStop = false;
+            // 
+            // GitHubRepLink
+            // 
+            this.GitHubRepLink.AutoSize = true;
+            this.GitHubRepLink.Cursor = System.Windows.Forms.Cursors.Default;
+            this.GitHubRepLink.LinkBehavior = System.Windows.Forms.LinkBehavior.HoverUnderline;
+            this.GitHubRepLink.Location = new System.Drawing.Point(209, 321);
+            this.GitHubRepLink.Name = "GitHubRepLink";
+            this.GitHubRepLink.Size = new System.Drawing.Size(63, 13);
+            this.GitHubRepLink.TabIndex = 7;
+            this.GitHubRepLink.TabStop = true;
+            this.GitHubRepLink.Text = "GitHub Rep";
+            this.GitHubRepLink.VisitedLinkColor = System.Drawing.Color.Blue;
+            this.GitHubRepLink.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.GitHubRepLink_LinkClicked);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(284, 351);
+            this.Controls.Add(this.GitHubRepLink);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.SongNameLabel);
+            this.Controls.Add(this.ArtworkPB);
+            this.Controls.Add(this.PlaylistModeCheckBox);
+            this.Controls.Add(this.RecordButton);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "TheBitBrine\'s Spotify Recorder ";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.ArtworkPB)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button RecordButton;
+        private System.Windows.Forms.Timer FetchTitleTimer;
+        private System.Windows.Forms.Timer FetchInfoTimer;
+        private System.Windows.Forms.Timer TrackWatchdog;
+        private System.Windows.Forms.CheckBox PlaylistModeCheckBox;
+        private System.Windows.Forms.PictureBox ArtworkPB;
+        private System.Windows.Forms.Label SongNameLabel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer UI_UpdaterTimer;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.LinkLabel GitHubRepLink;
+    }
+}
+
---
+++ SpotifyRecorder/Form1.cs
@@ -0,0 +1,369 @@
+﻿using NAudio.Lame;
+using NAudio.Wave;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+using SpotifyAPI;
+using SpotifyAPI.Local;
+using SpotifyAPI.Local.Enums;
+using SpotifyAPI.Local.Models;
+using TagLib;
+using System.Drawing.Imaging;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace SpotifyRecorder
+{
+    public partial class Form1 : Form
+    {
+        public static LameMP3FileWriter Writer;
+        public static IWaveIn waveIn;
+        public string ExportFolder;
+        public int BitRate = 320;
+
+        //-----Track Info-----
+        public string Artist;
+        public string Album;
+        public string TrackName;
+        public Bitmap Artwork;
+
+        //-----UI STUFF-----
+        public string SongNameFromWindowTitle = ""; //We need to read it for MP3 file name from the window title becuase local API is not fast enough.
+        public bool Busy = false;
+        public bool Playing = false;
+        public bool RecordPressed = false;
+        public bool RecordingCompleted = false;
+
+        //-----Spotify API-----
+        public static SpotifyLocalAPI SpotifyAPI;
+        public StatusResponse Status;
+
+        //-----Stats-----
+        public int TotalCount = 0;
+        public List<string> RecordedSongs = new List<string>();
+
+        //-----Threads-----
+        Thread MainThread;
+        ThreadStart MainThreadStart;
+
+
+        #region UI.Methods
+        public Form1()
+        {
+            InitializeComponent();
+            FetchNameFromTitle();
+            FireUpTheLocalAPI();
+            FetchTrackInfo();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            try /*To Kill Possible Remaining Threads*/
+            {
+                MainThread.Interrupt();
+                MainThread.Abort();
+                System.Environment.Exit(0);
+            }
+            catch
+            { /*Doesn't Matter*/}
+        }
+        
+        private void GitHubRepLink_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://github.com/thebitbrine/spotify-recorder");
+        }
+
+        private void Record_Click(object sender, EventArgs e)
+        {
+            FireUpTheLocalAPI();
+
+            if (Status.Volume < 1)
+            {
+                MessageBox.Show("Please set your Spotify Player's volume to 100% for the best results.", "Low Volume", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            if (RecordPressed == false)
+            {
+                //Start Recording (Threaded)
+                if (string.IsNullOrWhiteSpace(ExportFolder) == true)
+                {
+                    folderBrowserDialog1.ShowDialog();
+                    ExportFolder = folderBrowserDialog1.SelectedPath;
+                }
+                if (string.IsNullOrWhiteSpace(ExportFolder) == false)
+                {
+                    RecordStarter();
+                    RecordPressed = true;
+                    RecordButton.Text = "Stop";
+                }
+            }
+            else
+            {
+                SpotifyAPI.Pause();
+                Restart();
+            }
+        }
+
+        public void Restart()
+        {
+            ProcessStartInfo Info = new ProcessStartInfo();
+            Info.Arguments = "/C echo 1 && \"" + Application.ExecutablePath + "\"";
+            Info.WindowStyle = ProcessWindowStyle.Hidden;
+            Info.CreateNoWindow = true;
+            Info.FileName = "cmd.exe";
+            Process.Start(Info);
+            Application.Exit();
+        }
+        
+        #endregion
+
+        #region FetchInfo
+        public void FetchNameFromTitle()
+        {
+            Process[] AllPossibleProcesses = Process.GetProcessesByName("Spotify");
+            foreach (var MainProcess in AllPossibleProcesses)
+            {
+                if (string.IsNullOrWhiteSpace(MainProcess.MainWindowTitle) == false)
+                {
+                    SongNameFromWindowTitle = MainProcess.MainWindowTitle;
+                }
+            }
+        }
+
+        public void FireUpTheLocalAPI()
+        {
+            if (SpotifyLocalAPI.IsSpotifyRunning() == false)
+            {
+                SpotifyLocalAPI.RunSpotify();
+                while (SpotifyLocalAPI.IsSpotifyRunning() == false) { }
+            }
+
+            if (SpotifyLocalAPI.IsSpotifyWebHelperRunning() == false)
+            {
+                SpotifyLocalAPI.RunSpotifyWebHelper();
+                while (SpotifyLocalAPI.IsSpotifyWebHelperRunning() == false) { }
+            }
+
+            if (SpotifyAPI != null)
+            {
+                Status = SpotifyAPI.GetStatus();
+            }
+            else
+            {
+                localConnect();
+                Status = SpotifyAPI.GetStatus();
+            }
+        }
+        
+        public void localConnect()
+        {
+            SpotifyAPI = new SpotifyLocalAPI();
+            if (!SpotifyLocalAPI.IsSpotifyRunning())
+                return;
+            if (!SpotifyLocalAPI.IsSpotifyWebHelperRunning())
+                return;
+            if (!SpotifyAPI.Connect())
+                return;
+        }
+
+        public void FetchTrackInfo()
+        {
+            try
+            {
+                Artist = Status.Track.ArtistResource.Name;
+                Album = Status.Track.AlbumResource.Name;
+                TrackName = Status.Track.TrackResource.Name;
+                Artwork = Status.Track.GetAlbumArt(AlbumArtSize.Size640);
+            }
+            catch { }
+        }
+        #endregion
+        
+        #region Record & Tag
+
+        public void RecordStarter()
+        {
+            FetchNameFromTitle();
+            FireUpTheLocalAPI();
+            FetchTrackInfo();
+            MainThreadStart = new ThreadStart(Record);
+            MainThread = new Thread(MainThreadStart);
+            MainThread.IsBackground = true;
+            MainThread.Start();
+        }
+
+        public void Record()
+        {
+            //Begin No-GC
+            GC.KeepAlive(Writer);
+            GC.KeepAlive(waveIn);
+
+            FireUpTheLocalAPI();
+
+            SpotifyAPI.Pause();
+
+            //Time to sleep to prevent lower-level crashes dependent to LAME Library
+            Thread.Sleep(5000);
+
+            //Reset The Track Playing Position
+            SpotifyAPI.Skip();
+            Thread.Sleep(250); //Wait 250ms to prevent Spotify Player from losing its shit
+            SpotifyAPI.Previous();
+            SpotifyAPI.Pause();
+
+            RecordingCompleted = false;
+            Busy = true;
+            FetchTrackInfo();
+            string TempTrackName = TrackName;
+            string TempArtist = Artist;
+            string TempAlbum = Album;
+            Bitmap TempArtwork = Artwork;
+            Busy = false;
+
+            waveIn = new WasapiLoopbackCapture();
+            waveIn.DataAvailable += waveIn_DataAvailable;
+            waveIn.RecordingStopped += waveIn_RecordingStopped;
+
+            SpotifyAPI.ListenForEvents = true;
+
+
+            //READY?
+            while (SpotifyAPI.GetStatus().Playing == false)
+            {
+                SpotifyAPI.Play();
+                if (SpotifyAPI.GetStatus().Playing == false)
+                    Thread.Sleep(100);
+            }
+            
+            //AIM
+            FetchNameFromTitle();
+            while (SongNameFromWindowTitle == "Spotify") { }
+            string TempName = SongNameFromWindowTitle;
+
+            //Remove Illegal Characters From Path
+            string regexSearch = new string(Path.GetInvalidFileNameChars()) + new string(Path.GetInvalidPathChars());
+            Regex r = new Regex(string.Format("[{0}]", Regex.Escape(regexSearch)));
+            string Filename = ExportFolder + @"\" + r.Replace(TempName, "") + ".mp3";
+            Writer = new LameMP3FileWriter(Filename, waveIn.WaveFormat, LAMEPreset.INSANE, null);
+
+
+            //SHOOT!
+            waveIn.StartRecording();
+
+
+            while (TempName == SongNameFromWindowTitle) { FetchNameFromTitle(); }
+
+            //Skip And Previous For Setting The Track's Time Position To 0 And Pause The Song Until It Exits
+            SpotifyAPI.Skip();
+            SpotifyAPI.Previous();
+            SpotifyAPI.Pause();
+
+            //Flush, Write And Exit
+            waveIn.StopRecording();
+            Thread.Sleep(750);
+
+            Writer.Flush();
+            waveIn.Dispose();
+            Writer.Dispose();
+
+            //Tag With Correct Info
+            lock (TempArtwork)
+            TagTheMP3(Filename, TempTrackName, TempArtist, TempAlbum, (Bitmap)TempArtwork.Clone());
+            
+            //Pull Da Trigga
+            RecordingCompleted = true;
+            
+
+            //Finally Kill The Thread
+            MainThread.Abort();
+        }
+
+        public void TagTheMP3(string Filename, string _TrackName, string _Artist, string _Album, Bitmap _Artwork)
+        {
+            Busy = true;
+            //Initialize Info
+            if(_Album == _TrackName) { _Album = "Single"; }
+            Bitmap TagArtwork = new Bitmap(_Artwork);
+            TagLib.File TagFile = TagLib.File.Create(Filename);
+            TagFile.Tag.Title = _TrackName;
+            TagFile.Tag.Album = _Album;
+            TagFile.Tag.Artists = new string[] { _Artist };
+            TagFile.Tag.Comment = "Ripped From Spotify By TheBitBrine's Spotify Recorder";
+
+            //Artwork Stuff
+            TagLib.Picture TempArtwork = new TagLib.Picture();
+            TempArtwork.Type = TagLib.PictureType.FrontCover;
+            TempArtwork.Description = "Cover";
+            TempArtwork.MimeType = System.Net.Mime.MediaTypeNames.Image.Jpeg;
+            MemoryStream ms = new MemoryStream();
+            TagArtwork.Save(ms, System.Drawing.Imaging.ImageFormat.Jpeg);
+            ms.Position = 0;
+            TempArtwork.Data = TagLib.ByteVector.FromStream(ms);
+            TagFile.Tag.Pictures = new TagLib.IPicture[] { TempArtwork };
+
+            //Finally Write To File & Close Memory Stream
+            TagFile.Save();
+            ms.Close();
+            Busy = false;
+        }
+
+        private void SpotifyAPI_OnTrackChange(object sender, TrackChangeEventArgs e)
+        {
+            SpotifyAPI.Pause();
+        }
+
+        static void waveIn_RecordingStopped(object sender, StoppedEventArgs e)
+        {
+            // signal that recording has finished
+            //stopRecording = true;
+        }
+
+        static void waveIn_DataAvailable(object sender, WaveInEventArgs e)
+        {
+            // write recorded data to MP3 writer
+            if (Writer != null)
+                Writer.Write(e.Buffer, 0, e.BytesRecorded);
+        }
+
+        #endregion
+
+        #region Timers
+        private void FetchTitleTimer_Tick(object sender, EventArgs e)
+        {
+            FetchNameFromTitle();
+        }
+
+        private void TrackWatchdog_Tick(object sender, EventArgs e)
+        {
+            if (RecordingCompleted == true && MainThread.IsAlive == false && PlaylistModeCheckBox.Checked == true)
+               RecordStarter();
+        }
+
+        private void UI_UpdaterTimer_Tick(object sender, EventArgs e)
+        {
+            if (Artwork != null && Busy == false)
+            {
+                if(RecordPressed == true)
+                ArtworkPB.Image = Artwork;
+
+                if(SongNameFromWindowTitle == "Spotify")
+                {
+                    SongNameLabel.Text = Artist + " - " + TrackName + " (Paused)";
+                }
+                else
+                SongNameLabel.Text = SongNameFromWindowTitle;
+            }
+        }
+        #endregion
+        
+    }
+}
